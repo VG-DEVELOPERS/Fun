9f23c1b194be5c5916090679171fa995fd19d15a
VG-DEVELOPERS <152973395+VG-DEVELOPERS@users.noreply.github.com>
2025-05-09 15:53:48 +0530

    Update eval.py

--- shivu/modules/eval.py
+++ shivu/modules/eval.py
@@ -13,7 +13,7 @@ from telegram.ext import ContextTypes, CommandHandler
 from telegram.ext import CallbackContext 
 
 namespaces = {}
-DEV_LIST = [6534367642]
+DEV_LIST = [7717913705]
 
 def namespace_of(chat, update, bot):
     if chat not in namespaces:
