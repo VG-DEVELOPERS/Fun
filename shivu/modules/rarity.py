abefc142305921245852262c09bdeb56abd0db20
VG-DEVELOPERS <152973395+VG-DEVELOPERS@users.noreply.github.com>
2025-05-10 20:55:23 +0530

    Update rarity.py

--- shivu/modules/rarity.py
+++ shivu/modules/rarity.py
@@ -7,28 +7,25 @@ rarity_map = {
     "2": "🟣 Rare",
     "3": "🟡 Legendary",
     "4": "🟢 Medium",
-    "5": "🔮 limited edition",
-    "6": "⚜️ premium",
-    "7": "🎴 universal",
-    "8": "🎐 Crystal"
-
+    "5": "🔮 Limited edition",
+    "6": "⚜️ Premium",
+    "7": "🏖 Summer",
+    "8": "🎐 Crystal",
+    "9": "🫧 Mist",
+    "10": "❄️ Winter",
+    "11": "🍁 Autumn",
+    "12": "🌸 Festival"
 }
-
-    
-
-
 
 @shivuu.on_message(filters.command("rarity"))
 async def rarity_count(client: Client, message: Message):
     try:
         rarity_counts = {}
 
-        # Count the characters by rarity
         for rarity_id, rarity_name in rarity_map.items():
             count = await collection.count_documents({'rarity': rarity_name})
             rarity_counts[rarity_name] = count
 
-        # Create the message with the rarity counts
         rarity_message = "📊 𝗥𝗮𝗿𝗶𝘁𝘆 𝗖𝗼𝘂𝗻𝘁 📊\n\n"
         for rarity_name, count in rarity_counts.items():
             rarity_message += f"{rarity_name}: {count} characters\n"
@@ -37,3 +34,4 @@ async def rarity_count(client: Client, message: Message):
 
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
+        
